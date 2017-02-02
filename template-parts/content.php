49e6a7b971d811b142da460043a49d19fc98a522
blake03 <blake@edelta.net>
2017-02-02 17:44:15 -0600

    moved entry-meta into entry-footer

--- template-parts/content.php
+++ template-parts/content.php
@@ -20,9 +20,6 @@
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php deltacurrent_posted_on(); ?>
-		</div><!-- .entry-meta -->
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
@@ -43,6 +40,9 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+		<div class="entry-meta">
+			<?php deltacurrent_posted_on(); ?>
+		</div><!-- .entry-meta -->
 		<?php deltacurrent_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
